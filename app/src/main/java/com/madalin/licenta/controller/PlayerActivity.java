95cc2021f2f534a3851cbc6f2fd1f5c348306da2
Mădălin <59412123+MadalinDolca@users.noreply.github.com>
2022-05-18 00:08:28 +0300

    animatie imagine melodie + completion listener

--- app/src/main/java/com/madalin/licenta/controller/PlayerActivity.java
+++ app/src/main/java/com/madalin/licenta/controller/PlayerActivity.java
@@ -6,36 +6,27 @@ import static com.madalin.licenta.EdgeToEdge.edgeToEdge;
 import static com.madalin.licenta.controller.fragment.AcasaFragment.listaMelodii;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
-import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.ThemedSpinnerAdapter;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.WindowCompat;
 import androidx.palette.graphics.Palette;
 
 import com.bumptech.glide.Glide;
@@ -49,7 +40,7 @@ import com.madalin.licenta.model.Melodie;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PlayerActivity extends AppCompatActivity {
+public class PlayerActivity extends AppCompatActivity implements MediaPlayer.OnCompletionListener {
 
     LinearLayout linearLayoutContainer;
 
@@ -97,7 +88,7 @@ public class PlayerActivity extends AppCompatActivity {
             super.onBackPressed();
         });
 
-        new PlayerAsyncTask().execute(); // pregatire mediaPlayer si afisare date melodie
+        new PregatirePlayerAsyncTask().execute(); // pregatire mediaPlayer si afisare date melodie
     }
 
     @Override
@@ -124,6 +115,10 @@ public class PlayerActivity extends AppCompatActivity {
         nextThread.start();
     }
 
+    /**
+     * Porneste si opreste mediaPlayer-ul si schimba imaginea butonului PlayPause.
+     * Apeleaza {@link PlayerActivity#actualizareSeekBar()} pentru actualizarea seekBar-ului.
+     */
     private void playPauseBtnClicked() {
         // pauza player daca melodia este in curs de redare
         if (mediaPlayer.isPlaying()) {
@@ -136,21 +131,14 @@ public class PlayerActivity extends AppCompatActivity {
             mediaPlayer.start();
         }
 
-        seekBar.setMax(mediaPlayer.getDuration() / 1000);
-
-        // actualizare progres seekBar
-        PlayerActivity.this.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                if (mediaPlayer != null) {
-                    seekBar.setProgress(mediaPlayer.getCurrentPosition() / 1000);
-                }
-
-                handlerProgresMelodie.postDelayed(this, 1000);
-            }
-        });
+        actualizareSeekBar(); // actualizare progres seekBar
     }
 
+    /**
+     * Trece la melodia precedenta in functie de starea mediaPlayer-ului. Afiseaza datele noii
+     * melodii prin {@link PlayerActivity#afisareDateMelodie()}. Actualizeaza seekBar-ul prin
+     * {@link PlayerActivity#actualizareSeekBar()}. Apeleaza listener-ul {@link #onCompletion(MediaPlayer)}
+     */
     private void previousBtnClicked() {
         // porneste player cu noua melodie daca melodia precedenta este in curs de redare
         if (mediaPlayer.isPlaying()) {
@@ -162,11 +150,11 @@ public class PlayerActivity extends AppCompatActivity {
             mediaPlayer = MediaPlayer.create(getApplicationContext(), uri); // creare player cu noul URI
 
             afisareDateMelodie(); // actualizare UI cu datele noii melodii
+            actualizareSeekBar(); // actualizare progres seekBar
 
-            // actualizare progres seekBar
-            actualizareSeekBar();
+            mediaPlayer.setOnCompletionListener(this); // apelare listener pentru schimbarea melodiei la finalizare
 
-            floatingActionButtonPlayPause.setImageResource(R.drawable.ic_pause); // schimba imaginea butonului PlayPause in "pause"
+            floatingActionButtonPlayPause.setBackgroundResource(R.drawable.ic_pause); // schimba imaginea butonului PlayPause in "pause"
             mediaPlayer.start(); // porneste player-ul
         } else {
             mediaPlayer.stop(); // opreste player-ul
@@ -177,14 +165,19 @@ public class PlayerActivity extends AppCompatActivity {
             mediaPlayer = MediaPlayer.create(getApplicationContext(), uri); // creare player cu noul URI
 
             afisareDateMelodie(); // actualizare UI cu datele noii melodii
+            actualizareSeekBar(); // actualizare progres seekBar
 
-            // actualizare progres seekBar
-            actualizareSeekBar();
+            mediaPlayer.setOnCompletionListener(this); // apelare listener pentru schimbarea melodiei la finalizare
 
-            floatingActionButtonPlayPause.setImageResource(R.drawable.ic_play); // schimba imaginea butonului PlayPause in "play"
+            floatingActionButtonPlayPause.setBackgroundResource(R.drawable.ic_play); // schimba imaginea butonului PlayPause in "play"
         }
     }
 
+    /**
+     * Trece la melodia urmatoare in functie de starea mediaPlayer-ului. Afiseaza datele noii
+     * melodii prin {@link PlayerActivity#afisareDateMelodie()}. Actualizeaza seekBar-ul prin
+     * {@link PlayerActivity#actualizareSeekBar()}. Apeleaza listener-ul {@link #onCompletion(MediaPlayer)}
+     */
     private void nextBtnClicked() {
         // porneste player cu noua melodie daca melodia precedenta este in curs de redare
         if (mediaPlayer.isPlaying()) {
@@ -196,11 +189,11 @@ public class PlayerActivity extends AppCompatActivity {
             mediaPlayer = MediaPlayer.create(getApplicationContext(), uri); // creare player cu noul URI
 
             afisareDateMelodie(); // actualizare UI cu datele noii melodii
+            actualizareSeekBar(); // actualizare progres seekBar
 
-            // actualizare progres seekBar
-            actualizareSeekBar();
+            mediaPlayer.setOnCompletionListener(this); // apelare listener pentru schimbarea melodiei la finalizare
 
-            floatingActionButtonPlayPause.setImageResource(R.drawable.ic_pause); // // schimba imaginea butonului PlayPause in "pause"
+            floatingActionButtonPlayPause.setBackgroundResource(R.drawable.ic_pause); // // schimba imaginea butonului PlayPause in "pause"
             mediaPlayer.start(); // porneste player-ul
         } else {
             mediaPlayer.stop(); // opreste player-ul
@@ -211,15 +204,23 @@ public class PlayerActivity extends AppCompatActivity {
             mediaPlayer = MediaPlayer.create(getApplicationContext(), uri); // creare player cu noul URI
 
             afisareDateMelodie(); // actualizare UI cu datele noii melodii
+            actualizareSeekBar(); // actualizare progres seekBar
 
-            // actualizare progres seekBar
-            actualizareSeekBar();
+            mediaPlayer.setOnCompletionListener(this); // apelare listener pentru schimbarea melodiei la finalizare
 
-            floatingActionButtonPlayPause.setImageResource(R.drawable.ic_play); // schimba imaginea butonului PlayPause in "play"
+            floatingActionButtonPlayPause.setBackgroundResource(R.drawable.ic_play); // schimba imaginea butonului PlayPause in "play"
         }
     }
 
+    /**
+     * Seteaza valoarea maxima a seekBar-ului in functie de lungimea melodiei din mediaPlayer.
+     * Lanseaza un thread pe UiThread pentru actualizarea continua a progresului seekBar-ului in
+     * functie de pozitia curenta a mediaPlayer-ului.
+     */
     public void actualizareSeekBar() {
+        seekBar.setMax(mediaPlayer.getDuration() / 1000); // setare valoare maxima seekBar in functie de durata melodiei din mediaPlayer
+
+        // actualizeaza progresul seekBar-ului in functie de progresul mediaPlayer-ului
         PlayerActivity.this.runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -232,8 +233,25 @@ public class PlayerActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Listener pentru schimbarea melodiei dupa finalizarea acesteia. Apeleaza
+     * {@link PlayerActivity#nextBtnClicked()} pentru a trece la urmatoarea melodie. In cazul in
+     * care nu exista un mediaPlayer, creeaza unul.
+     */
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        nextBtnClicked(); // trece la urmatoarea melodie
+
+        // creeaza un nou mediaPlayer in cazul in care nu exista deja unul
+        if (mediaPlayer != null) {
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+            mediaPlayer.start();
+            mediaPlayer.setOnCompletionListener(this);
+        }
+    }
+
     @SuppressLint("StaticFieldLeak")
-    public class PlayerAsyncTask extends AsyncTask<Void, Void, Void> {
+    public class PregatirePlayerAsyncTask extends AsyncTask<Void, Void, Void> {
 
         @Override
         protected Void doInBackground(Void... voids) {
@@ -314,15 +332,22 @@ public class PlayerActivity extends AppCompatActivity {
                 }
             });
 
-            // listener repornire melodie dupa finalizare
-            mediaPlayer.setOnCompletionListener(mp -> {
-                seekBar.setProgress(0);
-                floatingActionButtonPlayPause.setImageResource(R.drawable.ic_play);
-                textViewDurataRedata.setText("0:00");
-                textViewDurataMelodie.setText("0:00");
-                mediaPlayer.reset();
-                pregatireMediaPlayer();
-            });
+//            // listener repornire melodie dupa finalizare
+//            mediaPlayer.setOnCompletionListener(mp -> {
+//                nextBtnClicked();
+//
+//                if (mediaPlayer != null) {
+//                    mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+//                    mediaPlayer.start();
+//                }
+//
+////                seekBar.setProgress(0);
+////                floatingActionButtonPlayPause.setImageResource(R.drawable.ic_play);
+////                textViewDurataRedata.setText("0:00");
+////                textViewDurataMelodie.setText("0:00");
+////                mediaPlayer.reset();
+////                pregatireMediaPlayer();
+//            });
         }
     }
 
@@ -398,7 +423,8 @@ public class PlayerActivity extends AppCompatActivity {
                     .into(new CustomTarget<Bitmap>() {
                         @Override
                         public void onResourceReady(@NonNull Bitmap resursaBitmapImagineMelodie, @Nullable Transition<? super Bitmap> transition) {
-                            imageViewImagineMelodie.setImageBitmap(resursaBitmapImagineMelodie); // setare bitmap obtinut ca imagine pentru melodie
+                            animatieImagine(PlayerActivity.this, imageViewImagineMelodie, resursaBitmapImagineMelodie);
+                            //imageViewImagineMelodie.setImageBitmap(resursaBitmapImagineMelodie); // setare bitmap obtinut ca imagine pentru melodie
 
                             // generare paleta de culori
                             Palette.from(resursaBitmapImagineMelodie).generate(palette -> {
@@ -407,14 +433,13 @@ public class PlayerActivity extends AppCompatActivity {
                                 if (swatchCuloareDominanta != null) {
                                     GradientDrawable gradientDrawable = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, new int[]{swatchCuloareDominanta.getRgb(), 0xFF000000}); // culoare fundal cu gradient
                                     linearLayoutContainer.setBackground(gradientDrawable); // setare culoare fundal
-                                    setareCuloriElemente(swatchCuloareDominanta);
+                                    setareCuloriElemente(swatchCuloareDominanta); // aplica culoarea swatch-ului pe elementele player-ului
                                 }
                             });
                         }
 
                         @Override
                         public void onLoadCleared(@Nullable Drawable placeholder) {
-
                         }
                     });
         }
@@ -469,6 +494,50 @@ public class PlayerActivity extends AppCompatActivity {
         timerString = timerString + minute + ":" + secundeString;
 
         return timerString;
+    }
+
+    // metoda pentru aplicarea unei animatii la tranzitia dintre imaginile melodiilor
+    public void animatieImagine(Context context, ImageView imageView, Bitmap bitmap) {
+        Animation animationOut = AnimationUtils.loadAnimation(context, android.R.anim.fade_out);
+        final Animation animationIn = AnimationUtils.loadAnimation(context, android.R.anim.fade_in);
+
+        animationOut.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Glide.with(context).load(bitmap).into(imageView);
+
+                animationIn.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+
+                imageView.startAnimation(animationIn);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        imageView.startAnimation(animationOut);
     }
 
     // metoda pentru initializarea vederilor
