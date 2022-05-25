587246e3985c53d06a1b4a29cc6aa6810ff2a953
Mădălin <59412123+MadalinDolca@users.noreply.github.com>
2022-05-25 23:08:39 +0300

    Modificare modele

---
+++ app/src/main/java/com/madalin/licenta/models/Utilizator.java
@@ -0,0 +1,14 @@
+package com.madalin.licenta.models;
+
+public class Utilizator {
+    public String nume;
+    public String email;
+
+    public Utilizator() {
+    }
+
+    public Utilizator(String nume, String email) {
+        this.nume = nume;
+        this.email = email;
+    }
+}
