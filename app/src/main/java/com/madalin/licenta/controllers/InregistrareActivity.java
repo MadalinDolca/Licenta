44cf6ce9e242f46e2cd20b8e3df5907f542fffc5
Mădălin <59412123+MadalinDolca@users.noreply.github.com>
2022-06-16 14:41:11 +0300

    dialog succes la inregistrare

--- app/src/main/java/com/madalin/licenta/controllers/InregistrareActivity.java
+++ app/src/main/java/com/madalin/licenta/controllers/InregistrareActivity.java
@@ -1,14 +1,16 @@
 package com.madalin.licenta.controllers;
 
-import static com.madalin.licenta.EdgeToEdge.Directie;
-import static com.madalin.licenta.EdgeToEdge.Spatiere;
-import static com.madalin.licenta.EdgeToEdge.edgeToEdge;
+import static com.madalin.licenta.global.EdgeToEdge.Directie;
+import static com.madalin.licenta.global.EdgeToEdge.Spatiere;
+import static com.madalin.licenta.global.EdgeToEdge.edgeToEdge;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.TextUtils;
 import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +20,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 import com.madalin.licenta.R;
 import com.madalin.licenta.models.Utilizator;
 
@@ -30,6 +33,7 @@ public class InregistrareActivity extends AppCompatActivity {
     ProgressDialog progressDialog;
 
     private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,7 @@ public class InregistrareActivity extends AppCompatActivity {
         edgeToEdge(this, findViewById(R.id.inregistrare_linearLayoutAutentificate), Spatiere.MARGIN, Directie.JOS);
 
         firebaseAuth = FirebaseAuth.getInstance(); // initializare Firebase Auth
+        firebaseDatabase = FirebaseDatabase.getInstance(); // initializare Firebase Database
 
         // obtinere vederi
         editTextNume = findViewById(R.id.inregistrare_editTextNume);
@@ -48,11 +53,16 @@ public class InregistrareActivity extends AppCompatActivity {
 
         // listeners
         buttonInregistreazate.setOnClickListener(view -> creazaUtilizator());
-        textViewAutentificate.setOnClickListener(view -> startActivity(new Intent(InregistrareActivity.this, AutentificareActivity.class)));
+        textViewAutentificate.setOnClickListener(view -> startActivity(new Intent(InregistrareActivity.this, AutentificareActivity.class))); // lansare AutentificareActivity la apasarea butonului
     }
 
+    /**
+     * Obtine si valideaza datele introduse in campuri. Daca datele sunt corecte, se creeaza noul
+     * utilizator Firebase folosind {@link FirebaseAuth#createUserWithEmailAndPassword(String, String)}.
+     * Daca crearea utilizatorului s-a efectuat cu succes, se adauga datele acestuia in baza de date.
+     */
     private void creazaUtilizator() {
-        // obtinere continut edit text
+        // obtinere continut din edit text
         String nume = editTextNume.getText().toString().trim();
         String email = editTextEmail.getText().toString().trim();
         String parola = editTextParola.getText().toString().trim();
@@ -85,34 +95,42 @@ public class InregistrareActivity extends AppCompatActivity {
         // daca datele sunt corecte se trece la inregistrarea in Firebase
         else {
             progressDialog = new ProgressDialog(InregistrareActivity.this); // initializare Progress Dialog
-            progressDialog.show(); // afisare dialog
-            progressDialog.setContentView(R.layout.layout_progress_dialog); // setare Content View
-            progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent); // fundal transparent
+            progressDialog.show(); // afiseaza progress dialog-ul
+            progressDialog.setContentView(R.layout.layout_progress_dialog); // seteaza continutul progress dialog-ului cu layout_progress_dialog
+            progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent); // seteaza fundalul progress dialog-ului ca transparent
 
-            firebaseAuth.createUserWithEmailAndPassword(email, parola)
-                    .addOnCompleteListener(taskCreareUtilizator -> {
-                        // daca s-a inregistrat cu succes
+            firebaseAuth.createUserWithEmailAndPassword(email, parola) // creeaza noul utilizator folosind adresa de email si parola
+                    .addOnCompleteListener(taskCreareUtilizator -> { // listener pentru efectuarea actiunilor la finalizarea crearii utilizatorului
+                        // daca utilizatorul s-a creat cu succes
                         if (taskCreareUtilizator.isSuccessful()) {
-                            Utilizator utilizator = new Utilizator(nume, email);
+                            Utilizator utilizator = new Utilizator(nume, email, ServerValue.TIMESTAMP); // memoreaza datele utilizatorului curent intr-un obiect Utilizator
 
-                            FirebaseDatabase.getInstance().getReference("Utilizatori")
-                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid()) // inregistrarii cu ID-ului respectiv
-                                    .setValue(utilizator) // i se atribuie datele din obiect
-                                    .addOnCompleteListener(taskAdaugareInBazaDeDate -> {
+                            firebaseDatabase.getReference("utilizatori") // obtine referinta spre "utilizatori"
+                                    .child(firebaseAuth.getCurrentUser().getUid()) // adauga child lui "utilizatori" avand ca nume UID-ul utilizatorului curent
+                                    .setValue(utilizator) // adauga in noul child datele din obiect
+                                    .addOnCompleteListener(taskAdaugareInBazaDeDate -> { // listener pentru efectuarea actiunilor la finalizarea adaugarii datelor utilizatorului in baza de date
+                                        // daca datele s-au adaugat cu succes in baza de date
                                         if (taskAdaugareInBazaDeDate.isSuccessful()) {
-                                            Toast.makeText(InregistrareActivity.this, "Te-ai inregistrat cu succes!", Toast.LENGTH_SHORT).show();
-                                            progressDialog.dismiss(); // inchidere Progress Dialog
+                                            progressDialog.findViewById(R.id.progress_dialog_progressBar).setVisibility(View.GONE); // ascunde spinner-ul
+                                            progressDialog.findViewById(R.id.progress_dialog_lottie).setVisibility(View.VISIBLE); // afiseaza animatia lottie
+                                            TextView textViewProgressDialog = progressDialog.findViewById(R.id.progress_dialog_textViewMesaj);
+                                            textViewProgressDialog.setText("Te-ai înregistrat cu succes!");
 
-                                            // lansare activitate login dupa inregistrare
-                                            startActivity(new Intent(InregistrareActivity.this, AutentificareActivity.class));
-                                        } else {
+                                            new Handler().postDelayed(() -> progressDialog.dismiss(), 3000); // // inlatura progress dialog-ul dupa 3 secunde
+
+                                            startActivity(new Intent(InregistrareActivity.this, AutentificareActivity.class)); // lanseaza AutentificareActivity dupa crearea utilizatorului si adaugarea datelor acestuia in baza de date
+                                        }
+                                        // daca datele nu s-au putut adauga
+                                        else {
                                             Toast.makeText(InregistrareActivity.this, "Eroare la inregistrare date: " + taskCreareUtilizator.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                                            progressDialog.dismiss();
+                                            progressDialog.dismiss(); // inlatura progress dialog-ul
                                         }
                                     });
-                        } else {
+                        }
+                        // daca utilizatorul nu s-a putut crea
+                        else {
                             Toast.makeText(InregistrareActivity.this, "Eroare la inregistrare: " + taskCreareUtilizator.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            progressDialog.dismiss();
+                            progressDialog.dismiss(); // inlatura progress dialog-ul
                         }
                     });
         }
