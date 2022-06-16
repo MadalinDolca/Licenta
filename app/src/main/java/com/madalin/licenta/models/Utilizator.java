449d13cc66bbe9b8d4375b3a15377523a82002bc
Mădălin <59412123+MadalinDolca@users.noreply.github.com>
2022-06-16 14:44:09 +0300

    modificare structura utilizator

--- app/src/main/java/com/madalin/licenta/models/Utilizator.java
+++ app/src/main/java/com/madalin/licenta/models/Utilizator.java
@@ -1,14 +1,19 @@
 package com.madalin.licenta.models;
 
+import java.util.Map;
+
 public class Utilizator {
     public String nume;
     public String email;
+    public String grad;
+    public Map<String, String> dataCrearii;
 
     public Utilizator() {
     }
 
-    public Utilizator(String nume, String email) {
+    public Utilizator(String nume, String email, Map<String, String> dataCrearii) {
         this.nume = nume;
         this.email = email;
+        this.dataCrearii = dataCrearii;
     }
 }
