9fb4ae87ad5446c6f8ffdc558ef150060f0c6974
Mădălin <59412123+MadalinDolca@users.noreply.github.com>
2022-06-17 22:59:09 +0300

    update activitate profil

--- app/src/main/java/com/madalin/licenta/controllers/ProfilActivity.java
+++ app/src/main/java/com/madalin/licenta/controllers/ProfilActivity.java
@@ -1,64 +1,111 @@
 package com.madalin.licenta.controllers;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import static com.madalin.licenta.global.EdgeToEdge.Spatiere;
+import static com.madalin.licenta.global.EdgeToEdge.edgeToEdge;
 
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.madalin.licenta.R;
+import com.madalin.licenta.adapters.BannerMelodieAdapter;
+import com.madalin.licenta.global.EdgeToEdge;
+import com.madalin.licenta.global.NumeExtra;
+import com.madalin.licenta.models.Melodie;
 import com.madalin.licenta.models.Utilizator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class ProfilActivity extends AppCompatActivity {
 
-    private FirebaseUser firebaseUser;
-    private DatabaseReference databaseReference;
-    private String userID;
+    private RecyclerView recyclerView;
+    private TextView textViewNumeUtilizator;
+    private TextView textViewGrad;
+    private TextView textViewNumarMelodii;
+    private TextView textViewNumarRedari;
 
-    private TextView textViewSalut;
-    private TextView textViewEmail;
-    private TextView textViewNume;
-    private Button button;
+    private DatabaseReference databaseReferenceUtilizatori;
+    private DatabaseReference databaseReferenceMelodii;
+    private String cheieUtilizator; // cheia din baza de date a utilizatorului
+    private List<Melodie> listaMelodii;
+    private int numarRedari;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
 
-        button = findViewById(R.id.profil_buttonMeniu);
+        initializareVederi(); // initializeaza vederile activitatii
+        edgeToEdge(this, findViewById(R.id.profil_scrollViewContainer), Spatiere.PADDING, EdgeToEdge.Directie.SUS);
+        edgeToEdge(this, recyclerView, Spatiere.MARGIN, EdgeToEdge.Directie.JOS);
 
-        // deconectare de la firebase
-        /*button.setOnClickListener(v -> afisareDialog());*/
+        listaMelodii = new ArrayList<>();
+        cheieUtilizator = getIntent().getStringExtra(NumeExtra.CHEIE_UTILIZATOR); // obtine cheia utilizatorului oferita in extra
+        databaseReferenceUtilizatori = FirebaseDatabase.getInstance().getReference("utilizatori");
+        databaseReferenceMelodii = FirebaseDatabase.getInstance().getReference("melodii");
 
-        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        databaseReference = FirebaseDatabase.getInstance().getReference("Utilizatori"); // referinta catre ...
-        userID = firebaseUser.getUid(); // obtinere ID utilizator
-
-        textViewSalut = findViewById(R.id.profil_textViewSalut);
-        textViewEmail = findViewById(R.id.profil_textViewEmail);
-        textViewNume = findViewById(R.id.bottom_sheet_meniu_textViewNume);
-
-        // obtinere date utilizator din baza de date in functie de ID-ul acestuia
-        databaseReference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+        // obtine datele utilizatorului din baza de date in functie de cheia acestuia
+        databaseReferenceUtilizatori.child(cheieUtilizator).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 // obtinere date din DB printr-un data snapshot si marshalling date in clasa Utilizator
                 Utilizator profilUtilizator = snapshot.getValue(Utilizator.class);
 
                 if (profilUtilizator != null) {
-                    textViewSalut.setText("Salut, " + profilUtilizator.getNume() + "!");
-                    textViewNume.setText(profilUtilizator.getNume());
-                    textViewEmail.setText(profilUtilizator.getEmail());
+                    textViewNumeUtilizator.setText(profilUtilizator.getNume());
+
+                    if (Objects.equals(profilUtilizator.getGrad(), "admin")) {
+                        textViewGrad.setText(profilUtilizator.getGrad());
+                    } else {
+                        textViewGrad.setText("utilizator");
+                    }
                 }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ProfilActivity.this, "Eroare: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // obtine melodiile care apartin utilizatorului selectat
+        databaseReferenceMelodii.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                listaMelodii.clear();
+                numarRedari = 0;
+
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) { // parcurge toti descendentii nodului "melodii" din baza de date
+                    Melodie melodie = dataSnapshot.getValue(Melodie.class); // adauga valorile descendentului in obiect
+                    System.out.println(numarRedari);
+
+                    if (Objects.equals(melodie.getCheieArtist(), cheieUtilizator)) { // daca cheia utilizatorului din melodie corespunde cu cheia utilizatorului selectat
+                        listaMelodii.add(melodie); // adauga datele melodiei in lista
+                        numarRedari += melodie.getNumarRedari(); // incrementeaza numarul de redari
+                        System.out.println(numarRedari);
+                    }
+                }
+
+                // afiseaza numarul total de melodii incarcat si numarul total de redari
+                textViewNumarMelodii.setText(listaMelodii.size() + " melodii încărcate");
+                textViewNumarRedari.setText(numarRedari + " redări totale");
+
+                // creeaza si seteaza adapter-ul pentru banner-ele cu melodii
+                BannerMelodieAdapter bannerMelodieAdapter = new BannerMelodieAdapter(ProfilActivity.this, listaMelodii); // creeaza adapter banner melodii
+                recyclerView.setLayoutManager(new LinearLayoutManager(ProfilActivity.this));
+                recyclerView.setAdapter(bannerMelodieAdapter); // setare adapter pe recyclerView pentru a furniza child views la cerere
             }
 
             @Override
@@ -68,30 +115,14 @@ public class ProfilActivity extends AppCompatActivity {
         });
     }
 
-/*
-    // metoda pentru afisarea dialogului meniu
-    private void afisareDialog() {
-        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(ProfilActivity.this, R.style.BottomSheetDialogTheme);
-        //bottomSheetDialog.setCanceledOnTouchOutside(false);
-
-        View bottomSheetView = LayoutInflater.from(getApplicationContext())
-                .inflate(R.layout.layout_bottom_sheet,
-                        (LinearLayout) findViewById(R.id.bottom_sheet_container));
-
-        // accesare activitate profil
-        bottomSheetView.findViewById(R.id.profil).setOnClickListener(view -> {
-            bottomSheetDialog.dismiss();
-            //startActivity(new Intent(MainActivity.this, ProfilActivity.class));
-        });
-
-        // deconectare de la firebase
-        bottomSheetView.findViewById(R.id.buttonDeconectare).setOnClickListener(view -> {
-            //firebaseAuth.signOut();
-            bottomSheetDialog.dismiss();
-            //startActivity(new Intent(MainActivity.this, AutentificareActivity.class));
-        });
-
-        bottomSheetDialog.setContentView(bottomSheetView); // setare continut bottom sheet
-        bottomSheetDialog.show(); // afisare bottom sheet
-    }*/
+    /**
+     * Initializeaza toate vederile din {@link ProfilActivity}.
+     */
+    private void initializareVederi() {
+        recyclerView = findViewById(R.id.profil_recyclerViewMelodii);
+        textViewNumeUtilizator = findViewById(R.id.profil_textViewNumeUtilizator);
+        textViewGrad = findViewById(R.id.profil_textViewGrad);
+        textViewNumarMelodii = findViewById(R.id.profil_textViewNumarMelodii);
+        textViewNumarRedari = findViewById(R.id.profil_textViewNumarRedari);
+    }
 }
