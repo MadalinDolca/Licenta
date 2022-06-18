5837326cccd62f7342f5447e3ed3fe95ae9cf8d3
Mădălin <59412123+MadalinDolca@users.noreply.github.com>
2022-06-18 20:15:07 +0300

    adaugare activitate

---
+++ app/src/main/java/com/madalin/licenta/controllers/SolicitaPermisiuneaActivity.java
@@ -0,0 +1,16 @@
+package com.madalin.licenta.controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.madalin.licenta.R;
+
+import android.os.Bundle;
+
+public class SolicitaPermisiuneaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicita_permisiunea);
+    }
+}
