a4effe4db63e6e312ec5d5354c922e391c4f906f
Mădălin <59412123+MadalinDolca@users.noreply.github.com>
2022-06-23 03:20:58 +0300

    Update NumeExtra.java

--- app/src/main/java/com/madalin/licenta/global/NumeExtra.java
+++ app/src/main/java/com/madalin/licenta/global/NumeExtra.java
@@ -11,4 +11,5 @@ public class NumeExtra {
     public static final String POZITIE_MELODIE_SERVICE = "com.madalin.licenta.POZITIE_MELODIE_SERVICE";
     public static final String ACTIUNE_NOTIFICARE = "com.madalin.licenta.ACTIUNE_NOTIFICARE";
     public static final String SOLICITA_PERMISIUNEA = "com.madalin.licenta.SOLICITA_PERMISIUNEA";
+    public static final String EVALUARE_SOLICITARE = "com.madalin.licenta.EVALUARE_SOLICITARE";
 }
