016d6319a7cc24125b110bccb740f1be4fb917f4
Mădălin <59412123+MadalinDolca@users.noreply.github.com>
2022-06-26 16:20:42 +0300

    cautare melodie in db

--- app/src/main/java/com/madalin/licenta/controllers/fragments/CautaFragment.java
+++ app/src/main/java/com/madalin/licenta/controllers/fragments/CautaFragment.java
@@ -2,65 +2,133 @@ package com.madalin.licenta.controllers.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.madalin.licenta.R;
+import com.madalin.licenta.adapters.BannerMelodieCautataAdapter;
+import com.madalin.licenta.adapters.CategorieCarduriAdapter;
+import com.madalin.licenta.models.Melodie;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CautaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class CautaFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private EditText editTextBaraCautare;
+    private ImageView imageViewButonCautare;
+    private RecyclerView recyclerView;
+    private TextView textViewMesaj;
+    private LottieAnimationView lottieAnimationView;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private List<Melodie> listaMelodii;
 
     public CautaFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CautaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CautaFragment newInstance(String param1, String param2) {
-        CautaFragment fragment = new CautaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        listaMelodii = new ArrayList<>();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cauta, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_cauta, container, false);
+        editTextBaraCautare = view.findViewById(R.id.cauta_editTextBaraCautare);
+        imageViewButonCautare = view.findViewById(R.id.cauta_imageViewButonCautare);
+        recyclerView = view.findViewById(R.id.cauta_recyclerView);
+        textViewMesaj = view.findViewById(R.id.cauta_textViewMesaj);
+        lottieAnimationView = view.findViewById(R.id.cauta_lottieAnimationView);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // cauta melodia la apasarea butonului ENTER in EditText
+        editTextBaraCautare.setOnKeyListener((v, keyCode, keyevent) -> {
+            if ((keyevent.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                cautaMelodii(editTextBaraCautare.getText().toString());
+                return true;
+            }
+            return false;
+        });
+
+        // cauta melodia la apasarea butonului de cautare
+        imageViewButonCautare.setOnClickListener(v -> cautaMelodii(editTextBaraCautare.getText().toString()));
+    }
+
+    /**
+     * Cauta melodia in baza de date in functie de termenul oferit. Daca datele melodiei corespund
+     * termenului, aceasta se adauga in {@link #listaMelodii}. Seteaza un adapter pentru
+     * {@link #recyclerView} cu {@link #listaMelodii}.
+     *
+     * @param termenCautare termenul pentru cautarea melodiei
+     */
+    private void cautaMelodii(String termenCautare) {
+        textViewMesaj.setVisibility(View.GONE); // ascunde mesajul
+        lottieAnimationView.setVisibility(View.VISIBLE); // afiseaza animatia de incarcare
+
+        FirebaseDatabase.getInstance().getReference("melodii")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        listaMelodii.clear();
+
+                        for (DataSnapshot melodieSnapshot : snapshot.getChildren()) { // parcurge toti descendentii nodului "melodii" din baza de date
+                            Melodie melodie = melodieSnapshot.getValue(Melodie.class); // adauga valorile descendentului in obiect
+                            melodie.setCheie(melodieSnapshot.getKey()); // memoreaza cheia descendentului
+
+                            // verifica daca datele melodiei corespunde termenului de cautare
+                            if (melodie.getNumeMelodie().toLowerCase().contains(termenCautare)
+                                    || melodie.getNumeArtist().toLowerCase().contains(termenCautare)
+                                    || melodie.getGenMelodie().toLowerCase().contains(termenCautare)) {
+                                listaMelodii.add(melodie); // adauga obiectul in lista
+                            }
+                        }
+
+                        // daca lista este goala se afiseaza un mesaj
+                        if (listaMelodii.size() == 0) {
+                            textViewMesaj.setVisibility(View.VISIBLE);
+                        }
+                        // altfel se configureaza adapter-ul si recyclerView-ul
+                        else {
+                            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                            recyclerView.setAdapter(new BannerMelodieCautataAdapter(getContext(), listaMelodii)); // setare adapter pe recyclerView pentru a furniza child views la cerere
+                        }
+
+                        lottieAnimationView.setVisibility(View.GONE); // ascunde animatia de incarcare
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(getContext(), "Eroare cautare: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
