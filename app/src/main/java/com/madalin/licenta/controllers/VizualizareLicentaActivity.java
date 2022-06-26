ab0b0643211f47bee0a9be3e27f979f4f69d36f1
Mădălin <59412123+MadalinDolca@users.noreply.github.com>
2022-06-26 16:23:28 +0300

    descarca fisierele

--- app/src/main/java/com/madalin/licenta/controllers/VizualizareLicentaActivity.java
+++ app/src/main/java/com/madalin/licenta/controllers/VizualizareLicentaActivity.java
@@ -3,16 +3,19 @@ package com.madalin.licenta.controllers;
 import static com.madalin.licenta.global.EdgeToEdge.edgeToEdge;
 
 import android.app.DownloadManager;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.webkit.CookieManager;
+import android.webkit.URLUtil;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,35 +23,41 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.github.barteksc.pdfviewer.PDFView;
 import com.madalin.licenta.R;
-import com.madalin.licenta.global.EdgeToEdge;
+import com.madalin.licenta.global.EdgeToEdge.Directie;
+import com.madalin.licenta.global.EdgeToEdge.Spatiere;
 import com.madalin.licenta.global.NumeExtra;
 import com.madalin.licenta.models.Licenta;
+import com.madalin.licenta.models.Melodie;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Serializable;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class VizualizareLicentaActivity extends AppCompatActivity {
 
-    LinearLayout toolbar;
-    RelativeLayout relativeLayoutDateMelodie;
-    ImageView imageViewImagineMelodie;
-    TextView textViewNumeMelodie;
-    TextView textViewNumeArtist;
-    TextView textViewDataLicentierii;
-    TextView textViewNumeBeneficiar;
-    PDFView pdfView;
-    LinearLayout linearLayoutButoane;
-    Button buttonDescarcaMelodia;
-    Button buttonDescarcaLicenta;
+    private LinearLayout toolbar;
+    private RelativeLayout relativeLayoutDateMelodie;
+    private ImageView imageViewImagineMelodie;
+    private TextView textViewNumeMelodie;
+    private TextView textViewNumeArtist;
+    private TextView textViewDataLicentierii;
+    private TextView textViewNumeBeneficiar;
+    private PDFView pdfView;
+    private LinearLayout linearLayoutButoane;
+    private Button buttonDescarcaMelodia;
+    private Button buttonDescarcaLicenta;
 
-    Licenta licenta;
+    private Licenta licentaSelectata; // memoreaza datele licentei selectate pentru afisare
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,54 +65,50 @@ public class VizualizareLicentaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_vizualizare_licenta);
 
         initializareVederi();
-        edgeToEdge(this, toolbar, EdgeToEdge.Spatiere.PADDING, EdgeToEdge.Directie.SUS);
-        edgeToEdge(this, linearLayoutButoane, EdgeToEdge.Spatiere.MARGIN, EdgeToEdge.Directie.JOS);
+        edgeToEdge(this, toolbar, Spatiere.PADDING, Directie.SUS);
+        edgeToEdge(this, linearLayoutButoane, Spatiere.PADDING, Directie.JOS);
 
-        licenta = (Licenta) getIntent().getSerializableExtra(NumeExtra.VIZUALIZARE_LICENTA); // obtine datele licentei din extra
+        licentaSelectata = (Licenta) getIntent().getSerializableExtra(NumeExtra.VIZUALIZARE_LICENTA); // obtine datele licentei din extra
 
         // seteaza datele in vederi
         Glide.with(this)
-                .load(licenta.getTemp_imagineMelodie())
+                .load(licentaSelectata.getMelodie().getImagineMelodie())
                 .apply(RequestOptions.centerCropTransform())
                 .placeholder(R.drawable.logo_music)
                 .error(R.drawable.ic_eroare)
                 .into(imageViewImagineMelodie);
-        textViewNumeMelodie.setText(licenta.getTemp_numeMelodie());
-        textViewNumeArtist.setText(licenta.getTemp_numeArtist());
-        textViewDataLicentierii.setText("Data licențierii: " + new SimpleDateFormat("dd.MM.yyyy").format(new Date(licenta.getDataCreariiLong())));
-        textViewNumeBeneficiar.setText(licenta.getTemp_numeBeneficiar());
+        textViewNumeMelodie.setText(licentaSelectata.getMelodie().getNumeMelodie());
+        textViewNumeArtist.setText(licentaSelectata.getMelodie().getNumeArtist());
+        textViewDataLicentierii.setText("Data licențierii: " + new SimpleDateFormat("dd.MM.yyyy").format(new Date(licentaSelectata.getDataCreariiLong())));
+        textViewNumeBeneficiar.setText(licentaSelectata.getBeneficiar().getNume());
 
-        new ObtinePDFdinURL().execute(licenta.getUrlLicenta());
+        new ObtinePDFdinURL().execute(licentaSelectata.getUrlLicenta());
 
         // listener lansare PlayerActivity la apasare
         relativeLayoutDateMelodie.setOnClickListener(v -> {
-            Intent intent = new Intent(this, PlayerActivity.class);
+            if (licentaSelectata.getMelodie() != null) {
+                List<Melodie> listaMelodie = new ArrayList<>();
+                listaMelodie.add(licentaSelectata.getMelodie()); // adauga melodia curenta intr-o lista
+
+                Intent intent = new Intent(VizualizareLicentaActivity.this, PlayerActivity.class);
+                intent.putExtra(NumeExtra.LISTA_MELODII, (Serializable) listaMelodie); // adauga lista cu melodia curenta in extra
+                intent.putExtra(NumeExtra.POZITIE_MELODIE, 0); // adauga pozitia melodiei selectate
+                startActivity(intent); // lanseaza PlayerActivity
+            }
         });
 
         // listener lansare ProfilActivity la apasarea numelui beneficiarului
         textViewNumeBeneficiar.setOnClickListener(v -> {
             Intent intent = new Intent(this, ProfilActivity.class);
-            intent.putExtra(NumeExtra.CHEIE_UTILIZATOR, licenta.getCheieBeneficiar());
+            intent.putExtra(NumeExtra.CHEIE_UTILIZATOR, licentaSelectata.getCheieBeneficiar());
             startActivity(intent);
         });
 
-        // listener descarcare melodie la apasarea butonului
-        buttonDescarcaMelodia.setOnClickListener(v -> {
-            DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-            Uri uri = Uri.parse(licenta.getCheieMelodie());
-            DownloadManager.Request request = new DownloadManager.Request(uri);
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
-            long reference = manager.enqueue(request);
-        });
+        // listener descarcare melodiei la apasarea butonului
+        buttonDescarcaMelodia.setOnClickListener(v -> descarcaFisier(licentaSelectata, new Melodie()));
 
         // listener descarcare licenta la apasarea butonului
-        buttonDescarcaLicenta.setOnClickListener(v -> {
-            DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-            Uri uri = Uri.parse(licenta.getUrlLicenta());
-            DownloadManager.Request request = new DownloadManager.Request(uri);
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
-            long reference = manager.enqueue(request);
-        });
+        buttonDescarcaLicenta.setOnClickListener(v -> descarcaFisier(licentaSelectata, new Licenta()));
     }
 
     /**
@@ -136,6 +141,62 @@ public class VizualizareLicentaActivity extends AppCompatActivity {
         protected void onPostExecute(InputStream inputStream) {
             pdfView.fromStream(inputStream).load(); // adauga PDF-ul in vedere
         }
+    }
+
+    /**
+     * Identifica tipul obiectului transmis si obtine datele fisierului pe baza acestuia. Descarca
+     * fisierul pe baza datelor si il adauga intr-un director.
+     *
+     * @param licenta obiectul din care se vor extrage datele
+     * @param obiect  obiectul folosit pentru identificarea tipului fisierului
+     * @param <Tip>   tipul obiectului
+     */
+    private <Tip> void descarcaFisier(Licenta licenta, Tip obiect) {
+        String url = null;
+        String prefix = "Fisier";
+        String numeDirector;
+        String numeFisier = "Nume fisier";
+
+        // verifica tipul de obiect transmis ca parametru
+        if (obiect instanceof Licenta) {
+            //licenta = (Licenta) obiect;
+            url = licenta.getUrlLicenta();
+            numeFisier = URLUtil.guessFileName(url, null, null);
+            prefix = "Licenta ";
+        } else if (obiect.getClass() == Melodie.class) {
+            String numeFisierTemp;
+            String extensie;
+
+            //melodie = (Melodie) obiect;
+            url = licenta.getMelodie().getUrlMelodie(); // obtine adresa URL a melodiei
+            numeFisierTemp = URLUtil.guessFileName(url, null, null); // obtine numele melodiei din adresa URL
+            extensie = numeFisierTemp.substring(numeFisierTemp.lastIndexOf(".")); // obtine extensia fisierului din numele melodiei
+            numeFisier = licenta.getMelodie().getNumeMelodie()
+                    + " - "
+                    + licenta.getMelodie().getNumeArtist()
+                    + extensie;
+            prefix = "";
+        }
+
+        // seteaza numele datelor
+        numeDirector = "Pachet "
+                + new SimpleDateFormat("dd-MM-yyyy").format(new Date(licenta.getDataCreariiLong()))
+                + " - " + licenta.getMelodie().getNumeMelodie();
+
+        // cererea de descarcare
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url)); // cerere pentru descarcarea URL-ului
+        request.setTitle(prefix + numeFisier); // titlul descarcarii din notificare
+        request.setDescription("Se descarcă fișierul ..."); // descrierea descarcarii din notificare
+        request.addRequestHeader("cookie", CookieManager.getInstance().getCookie(url)); // adauga in header-ul cererii adresa fisierului drep cookie
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); // afiseaza notificarea de descarcare
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, File.separator + "Licente" + File.separator + numeDirector + File.separator + prefix + numeFisier); // destinatia de descarcare a fisierului
+        //request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE);
+
+        // descarcarea fisierului
+        DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE); // obtine serviciul de descarcare
+        downloadManager.enqueue(request); // descarca fisierul din cererea DownloadManager-ului
+
+        Toast.makeText(this, "Descărcarea a început!", Toast.LENGTH_SHORT).show();
     }
 
     /**
